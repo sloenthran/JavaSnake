5424c920929deba8ac99d59ee52f808c0c43bf63
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-04-02 00:59:24 +0300

    saving_and_look

--- src/main/java/pl/nogacz/snake/application/PauseGame.java
+++ src/main/java/pl/nogacz/snake/application/PauseGame.java
@@ -7,11 +7,19 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import pl.nogacz.snake.board.Board;
+import pl.nogacz.snake.board.Coordinates;
+import pl.nogacz.snake.pawn.Pawn;
+import pl.nogacz.snake.pawn.PawnClass;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.Random;
+import java.util.zip.CRC32;
+
 import static pl.nogacz.snake.board.Board.isPressedP;
 
 public class PauseGame {
@@ -41,7 +49,39 @@ public class PauseGame {
             System.out.println(path);
             try {
                 FileWriter writer=new FileWriter(path+"/save.txt");
+                //Data starts
+              //  Design design= board.getDesign();
+                int direction = 1;Board.getDirection();
+                int tailLength = board.getTailLength();
+            //add this while loading    HashMap<Coordinates, PawnClass> map= board.getMap();
+                /*PawnClass snakeHeadClass = board.getSnakeHeadClass();
+                PawnClass snakeBodyClass = board.getSnakeBodyClass();
+                PawnClass foodClass = board.getFoodClass();
 
+                 */
+                ArrayList<Coordinates> snakeTail = board.getSnakeTail();
+                Coordinates snakeHeadCoordinates = board.getSnakeHeadCoordinates();
+                //Data ends
+                CRC32 crc= new CRC32();
+
+                crc.update(direction);
+                String data=direction+"_"+crc.getValue()+"_";
+
+                crc.update(tailLength);
+                data+=tailLength+"_"+crc.getValue()+"_";
+
+                for(Coordinates c: snakeTail){
+                    crc.update(c.getX());
+                    data+=c.getX()+"_"+crc.getValue()+"_";
+                    crc.update(c.getY());
+                    data+=c.getY()+"_"+crc.getValue()+"_";
+                }
+                crc.update(snakeHeadCoordinates.getX());
+                data+=snakeHeadCoordinates.getX()+"_"+crc.getValue()+"_";
+
+                crc.update(snakeHeadCoordinates.getY());
+                data+=snakeHeadCoordinates.getY()+"_"+crc.getValue();
+                writer.write(data);
                 writer.close();
                 System.exit(0);
             } catch (IOException e) {
--- src/main/java/pl/nogacz/snake/board/Board.java
+++ src/main/java/pl/nogacz/snake/board/Board.java
@@ -9,6 +9,7 @@ import pl.nogacz.snake.application.EndGame;
 import pl.nogacz.snake.application.PauseGame;
 import pl.nogacz.snake.pawn.Pawn;
 import pl.nogacz.snake.pawn.PawnClass;
+import sun.security.krb5.internal.crypto.Des;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -224,4 +225,12 @@ public class Board {
     }
 
     public Board getBoard(){return this;}
+    public HashMap<Coordinates, PawnClass> getMap(){return this.board;}
+    public Design getDesign(){return this.design;}
+    public int getTailLength(){return this.tailLength;}
+    public PawnClass getSnakeHeadClass(){ return this.snakeHeadClass;}
+    public PawnClass getFoodClass(){ return this.snakeHeadClass;}
+    public PawnClass getSnakeBodyClass(){return this.snakeBodyClass;}
+    public Coordinates getSnakeHeadCoordinates(){return this.snakeHeadCoordinates;}
+    public ArrayList<Coordinates> getSnakeTail(){return this.snakeTail;}
 }
