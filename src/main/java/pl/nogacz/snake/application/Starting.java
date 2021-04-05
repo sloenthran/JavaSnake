c2490b172a4e625ac1e09f7e0ccbdde165dc995f
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-04-05 23:08:26 +0300

    buttons

--- src/main/java/pl/nogacz/snake/application/Starting.java
+++ src/main/java/pl/nogacz/snake/application/Starting.java
@@ -1,22 +1,51 @@
 package pl.nogacz.snake.application;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import pl.nogacz.snake.board.Coordinates;
+import pl.nogacz.snake.pawn.Pawn;
+import pl.nogacz.snake.pawn.PawnClass;
 
 public class Starting {
-    public GridPane gridPane = new GridPane();
+    public GridPane gridPane;
 
     public Starting() {
-        createBoardBackground();
+        Design d=new Design();// some desigs
+        for(int i = 0; i < 22; i++) {
+            d.addPawn(new Coordinates(0, i), new PawnClass(Pawn.BRICK));
+            d.addPawn(new Coordinates(21, i), new PawnClass(Pawn.BRICK));
+            d.addPawn(new Coordinates(i, 0), new PawnClass(Pawn.BRICK));
+            d.addPawn(new Coordinates(i, 21), new PawnClass(Pawn.BRICK));
+            if(i>=2 &&i<=20){
+                d.addPawn(new Coordinates(1,i),new PawnClass(Pawn.SNAKE_BODY));
+                d.addPawn(new Coordinates(20,i),new PawnClass(Pawn.SNAKE_BODY));
+            }
+        }
+        d.addPawn(new Coordinates(1,1),new PawnClass(Pawn.SNAKE_HEAD));
+        d.addPawn(new Coordinates(20,1),new PawnClass(Pawn.SNAKE_HEAD));
+
+        gridPane=d.getGridPane();
+        createStartingMenu();
     }
 
-    private void createBoardBackground() {
-        Image background = new Image(Resources.getPath("background.jpg"));
-        BackgroundSize backgroundSize = new BackgroundSize(715, 715, false, false, true, false);
-        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
-        gridPane.setBackground(new Background(backgroundImage));
-        //do this.
-        gridPane.add(new Button(),300,300);
+    private void createStartingMenu() {
+        Label label=new Label("Do you want to load a game or start a new game?");
+        final double MAX_FONT_SIZE = 25.0; // define max font size you need
+        label.setFont(new Font(MAX_FONT_SIZE)); // set to Label
+        gridPane.add(label,1,5,20,1);
+        Button b1=new Button("Load Game");
+        b1.setFont(new Font(MAX_FONT_SIZE));
+        Button b2=new Button("New Game");
+        b2.setFont(new Font(MAX_FONT_SIZE));
+        gridPane.add(b1,5,10,7,4);
+        gridPane.add(b2,10,10,7,4);
+
     }
 }
