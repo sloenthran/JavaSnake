1a7504e374b0412c2e1b58d3f4e2c1b614ca2cee
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-04-05 23:51:33 +0300

    added_new_game

--- src/main/java/pl/nogacz/snake/Snake.java
+++ src/main/java/pl/nogacz/snake/Snake.java
@@ -2,11 +2,15 @@ package pl.nogacz.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import pl.nogacz.snake.application.Design;
 import pl.nogacz.snake.application.Starting;
 import pl.nogacz.snake.board.Board;
+
+import java.util.Optional;
 
 /**
  * @author Dawid Nogacz on 19.05.2019
@@ -30,7 +34,8 @@ public class Snake extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 */
-        primaryStage.setScene(new Scene(new Starting().gridPane,715,715,Color.BLACK));
+        Starting start=new Starting(primaryStage);
+        primaryStage.setScene(new Scene(start.gridPane,715,715,Color.BLACK));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
--- src/main/java/pl/nogacz/snake/application/Starting.java
+++ src/main/java/pl/nogacz/snake/application/Starting.java
@@ -1,23 +1,29 @@
 package pl.nogacz.snake.application;
 
-import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import pl.nogacz.snake.board.Board;
 import pl.nogacz.snake.board.Coordinates;
 import pl.nogacz.snake.pawn.Pawn;
 import pl.nogacz.snake.pawn.PawnClass;
 
+
+
 public class Starting {
     public GridPane gridPane;
+    Stage s;
+    final double MAX_FONT_SIZE = 25.0;
 
-    public Starting() {
-        Design d=new Design();// some desigs
+    public Starting(Stage s) {
+        this.s=s;
+        Design d=new Design();// some designs for starting scene
         for(int i = 0; i < 22; i++) {
             d.addPawn(new Coordinates(0, i), new PawnClass(Pawn.BRICK));
             d.addPawn(new Coordinates(21, i), new PawnClass(Pawn.BRICK));
@@ -37,15 +43,36 @@ public class Starting {
 
     private void createStartingMenu() {
         Label label=new Label("Do you want to load a game or start a new game?");
-        final double MAX_FONT_SIZE = 25.0; // define max font size you need
-        label.setFont(new Font(MAX_FONT_SIZE)); // set to Label
+        label.setFont(new Font(MAX_FONT_SIZE));
+
         gridPane.add(label,1,5,20,1);
+
         Button b1=new Button("Load Game");
         b1.setFont(new Font(MAX_FONT_SIZE));
+
         Button b2=new Button("New Game");
         b2.setFont(new Font(MAX_FONT_SIZE));
+
         gridPane.add(b1,5,10,7,4);
         gridPane.add(b2,10,10,7,4);
+
+        b1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("save");
+                System.exit(0);
+            }
+        });
+        b2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                Design design=new Design();
+                Board board=new Board(design);
+                Scene scene = new Scene(design.getGridPane(), 715, 715, Color.BLACK);
+                scene.setOnKeyReleased(event -> board.readKeyboard(event));
+                s.setTitle("JavaSnake");
+                s.setScene(scene);
+            }
+        });
+
 
     }
 }
