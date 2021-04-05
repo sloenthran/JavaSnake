05d31b80566ed4f9c9117e94f5b3e9cf4c8ba4ab
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-04-06 01:27:37 +0300

    finish2

--- src/main/java/pl/nogacz/snake/Snake.java
+++ src/main/java/pl/nogacz/snake/Snake.java
@@ -2,22 +2,14 @@ package pl.nogacz.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import pl.nogacz.snake.application.Design;
 import pl.nogacz.snake.application.Starting;
-import pl.nogacz.snake.board.Board;
-
-import java.util.Optional;
 
 /**
  * @author Dawid Nogacz on 19.05.2019
  */
 public class Snake extends Application {
-    //Design design = new Design();
-    //Board board = new Board(design);
 
     public static void main(String[] args) {
         launch(args);
